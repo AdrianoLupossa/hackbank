1fbec5c665d2c1cb20a7883a1e38544749181878
adrianolupossa <adrianolupossa@gmail.com>
2022-06-18 15:21:09 +0100

    update footer background color

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,14 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background-color: #333;
+  color: #fff;
+`;
+
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white p-2 w-100 d-flex align-items-center justify-content-center">
+    <StyledFooter className="p-2 w-100 d-flex align-items-center justify-content-center">
       <span>Hackbank 2022 &copy; Todos os direitos reservados</span>
-    </footer>
+    </StyledFooter>
   );
 }
